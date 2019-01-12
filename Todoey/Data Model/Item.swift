eb309eeb13d275984a2d6fe3c559e38aee57bf32
Kilpatrick Atta-Poku <kil@kilpatricks-mbp.mshome.net>
2019-01-12 11:07:26 +0000

    Convert to MVC design Pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kilpatrick Atta-Poku on 12/01/2019.
+//  Copyright © 2019 KILTech. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
